342ac0c251607bdce9024614c0273312dab4cec9
Tarek3222 <00tarek404@gmail.com>
2024-09-30 19:42:43 +0300

    add assets and colors

---
+++ lib/core/constants/assets_images.dart
@@ -0,0 +1,3 @@
+abstract class AssetsImages {
+  static const logo = 'assets/images/logo.png';
+}
--- lib/core/themes/styles.dart
+++ lib/core/themes/styles.dart
@@ -1,1 +1,15 @@
+import 'package:clone_chat/core/themes/app_colors.dart';
+import 'package:flutter/material.dart';
 
+abstract class Styles {
+  static TextStyle textStyle24 = const TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle textStyle15 = const TextStyle(
+    fontSize: 15,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w600,
+  );
+}

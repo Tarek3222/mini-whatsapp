ff467c934327b1ba9be4ae4b9850c54deed949c6
Tarek3222 <00tarek404@gmail.com>
2024-11-08 01:18:24 +0200

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:clone_chat/core/constants/app_routers.dart';
+import 'package:clone_chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MiniChatApp());
 }
 
